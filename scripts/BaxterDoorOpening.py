a07189bd04bfb47e826823c347bb36910697db28
Jiangeng Dong <dongjiangeng@gmail.com>
2020-01-20 10:54:28 -0800

    Add argument checking in OMPLInterface

--- scripts/BaxterDoorOpening.py
+++ scripts/BaxterDoorOpening.py
@@ -17,8 +17,8 @@ import numpy as np
 import rospkg
 import openravepy as orpy
 
-from utils import SerializeTransform, RPY2Transform, quat2Transform, pause
-from plannerParameters import PlannerParameters
+import utils
+from OMPLInterface import OMPLInterface
 
 
 class BaxterDoorOpeningProblem:
@@ -29,7 +29,7 @@ class BaxterDoorOpeningProblem:
         self.env.SetDebugLevel(orpy.DebugLevel.Info)
 
         # load kitchen scene, delete herb robot, and load baxter robot
-        self.env.Load('scenes/intelkitchen_robotized_herb2.env.xml')
+        self.env.Load('environments/intelkitchen_robotized_herb2.env.xml')
         rp = rospkg.RosPack()
         baxter_path = rp.get_path('baxter_description')
         urdf_path = os.path.join(baxter_path, "urdf/baxter_sym.urdf")
@@ -47,11 +47,11 @@ class BaxterDoorOpeningProblem:
 
         self.kitchen = self.env.GetKinBody("kitchen")
 
-        self.planner = orpy.RaveCreatePlanner(self.env, 'AtlasMPNet')
+        self.planner = OMPLInterface(self.env, self.baxter)
         self.cbirrt = orpy.RaveCreateProblem(self.env, "CBiRRT")
         self.env.LoadProblem(self.cbirrt, name)
 
-        self.traj = orpy.RaveCreateTrajectory(self.env, '')
+        self.traj = None
 
         self.gotoInitialPose()
 
@@ -69,7 +69,7 @@ class BaxterDoorOpeningProblem:
                 raise Exception("Invalid manipulator!")
             self.baxter.SetActiveManipulator(manipulator)
             q_str = self.cbirrt.SendCommand('DoGeneralIK exec nummanips 1 maniptm %d %s' %
-                                            (self.baxter.GetActiveManipulatorIndex(), SerializeTransform(pose)))
+                                            (self.baxter.GetActiveManipulatorIndex(), utils.SerializeTransform(pose)))
         return [float(q) for q in q_str.split()]
 
     def setLeftArmConfig(self, leftarm_q):
@@ -100,31 +100,8 @@ class BaxterDoorOpeningProblem:
     def openHands(self):
         self.setHandsConfig([1, 1])
 
-    def setPlannerParameters(self, start_config, goal_config):
-        params = orpy.Planner.PlannerParameters()
-        params.SetRobotActiveJoints(self.baxter)
-        params.SetInitialConfig(start_config)
-        params.SetGoalConfig(goal_config)
-        # TODO: the extra parameter is fixed now. Make it more flexible.
-        # TODO: change cpp code of TSRRobot to support relative body and manipulator index
-        params.SetExtraParameters(
-            """<solver_parameters type="2" time="10" range="0.05"/>
-               <constraint_parameters type="1" tolerance="0.001" max_iter="50" delta="0.05" lambda="2"/>
-               <atlas_parameters exploration="0.5" epsilon="0.01" rho="0.08" alpha="0.45" max_charts="500" using_bias="1" separate="0"/>
-               <tsr_chain purpose="0 0 1" mimic_body_name="kitchen" mimic_body_index="6">
-                    <tsr manipulator_index="0" relative_body_name="NULL" 
-                        T0_w="-1  0  0  -0  -1  0  0  0  1  1.818  0.0266  0.884" 
-                        Tw_e="1  0  0  0  1  0  0  0  1  0.3829  0.0816  0.2801" 
-                        Bw="0  0  0  0  0  0  0  0  0  0  0 3.1415926" />
-                    <tsr manipulator_index="0" relative_body_name="NULL"
-                        T0_w="1  0  0  0  1  0  0  0  1  0  0  0"
-                        Tw_e="0  0  1  -1  0  0  0  -1  0  0  0.19500  0"
-                        Bw="0  0  0  0  0  0  0  0  0  0  -1.57  0" />
-               </tsr_chain>""")
-        return params
-
     def gotoInitialPose(self):
-        baxter_pose = np.array(RPY2Transform(0, 0, np.pi / 2, 0, 0, 0) * RPY2Transform(0, 0, 0, -0.8, -1.2217, 0.9614))
+        baxter_pose = np.array(utils.RPY2Transform(0, 0, np.pi / 2, 0, 0, 0) * utils.RPY2Transform(0, 0, 0, -0.8, -1.2217, 0.9614))
         self.baxter.SetTransform(baxter_pose)
 
         self.baxter.SetActiveDOFs(self.right_arm_indices)
@@ -134,14 +111,10 @@ class BaxterDoorOpeningProblem:
         self.setArmsConfig(joint_val[:7], joint_val[7:])
         self.openHands()
 
-    def solve(self, arm_start_pose, arm_goal_pose):
-        right_start = self.inverseKinematic(self.right_arm, arm_start_pose)
-        right_goal = self.inverseKinematic(self.right_arm, arm_goal_pose)
-        params = self.setPlannerParameters(right_start, right_goal)
-        with self.env, self.baxter:
-            self.planner.InitPlan(self.baxter, params)
-            self.planner.PlanPath(self.traj)
-        orpy.planningutils.RetimeTrajectory(self.traj)
+    def solve(self, arm_start_pose, arm_goal_pose, T0_w, Tw_e, Bw):
+        start_config = self.inverseKinematic(self.right_arm, arm_start_pose)
+        goal_config = self.inverseKinematic(self.right_arm, arm_goal_pose)
+        status, time, self.traj = self.planner.solve(start_config, goal_config, T0_w, Tw_e, Bw)
         return self.traj
 
     def display(self):
@@ -158,27 +131,39 @@ def main():
                                 [0, -1, 0, -0.055],
                                 [0, 0, 1, 1.1641],
                                 [0, 0, 0, 1]])
-    arm_start_pose = np.mat([[0, 1, 0, 1.4351],
-                             [0, 0, 1, -0.25],
-                             [1, 0, 0, 1.1641],
-                             [0, 0, 0, 1]])
-    handle_hinge_offset = hinge_start_pose.I * handle_start_pose
-    hand_handle_offset = handle_start_pose.I * arm_start_pose
+    hand_start_pose = np.mat([[0, 1, 0, 1.4351],
+                              [0, 0, 1, -0.25],
+                              [1, 0, 0, 1.1641],
+                              [0, 0, 0, 1]])
 
-    Thinge_rot = RPY2Transform(0, 0, np.pi / 2, 0, 0, 0)
-    Thandle_rot = RPY2Transform(0, 0, -np.pi / 4, 0, 0, 0)
+    handle_hinge_offset = hinge_start_pose.I * handle_start_pose
+    hand_handle_offset = handle_start_pose.I * hand_start_pose
+    hand_hinge_offset = hinge_start_pose.I *hand_start_pose
+
+    Thinge_rot = utils.RPY2Transform(0, 0, np.pi / 3, 0, 0, 0)
+    Thandle_rot = utils.RPY2Transform(0, 0, 0, 0, 0, 0)
 
     hinge_goal_pose = hinge_start_pose * Thinge_rot
+    hand_goal_pose = hinge_goal_pose * hand_hinge_offset
 
-    arm_goal_pose = hinge_goal_pose * handle_hinge_offset * Thandle_rot * hand_handle_offset
+    hinge_bound = np.array([[0, 0],
+                            [0, 0],
+                            [0, 0],
+                            [0, 0],
+                            [0, 0],
+                            [0, 3.1415928]])
+    handle_bound = np.array([[0, 0],
+                             [0, 0],
+                             [0, 0],
+                             [0, 0],
+                             [0, 0],
+                             [-1.58, 0]])
 
     problem = BaxterDoorOpeningProblem()
-    problem.solve(arm_start_pose, arm_goal_pose)
+    problem.solve(hand_start_pose, hand_goal_pose, hinge_start_pose, hand_hinge_offset, hinge_bound)
     problem.display()
-    pause()
+    utils.pause()
 
 
 if __name__ == '__main__':
     main()
-    # TODO: multiple TSRChains for different manipulators
-    # TODO: multiple TSRChains for the same manipulators to choose from
